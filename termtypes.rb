df12eaf5e71c5e3084f3788337ccc55691743ff5
AtLab-jp <abe12@mccc.jp>
2019-11-23 23:50:29 +0900

    文字表示

--- termtypes.rb
+++ termtypes.rb
@@ -1,5 +1,6 @@
 require './console.rb'
 require './utility.rb'
+require 'csv'
 
 
 class TermTypes
@@ -8,7 +9,14 @@ class TermTypes
   def initialize
     ## -----*----- コンストラクタ -----*----- ##
     @con = Console.new('./config/console.txt')
-    draw('test01', 'test02')
+
+
+    ## ===== test ===============
+    read_csv.each do |col|
+      draw(col[:text], col[:key], '')
+      sleep 1
+    end
+    ## ==========================
   end
 
   def draw(*msg)
@@ -19,6 +27,14 @@ class TermTypes
   def timer(framerate)
     ## -----*----- タイマー設定-----*----- ##
     Timer::set_frame_rate(20)
+  end
+
+  def read_csv
+    ## -----*----- CSV読み込み -----*----- ##
+    data = CSV.read('./config/text.csv')
+    data.shift
+
+    return data.map { |col| {text: col[0], key: col[1]}}
   end
 end
 
