c780ec2cc0d2d01ae982d7f5e20238f00b1aa80a
AjxLab <abe12@mccc.jp>
2020-01-27 01:20:02 +0900

    ローマ字->カタカナ変換

---
+++ test/to_katakana.rb
@@ -0,0 +1,34 @@
+require 'csv'
+
+
+str = 'すぺしゃるぷらいす'
+
+data = CSV.read('./config/romaji.csv')
+data.shift
+romaji = {}
+data.each { |col|
+  romaji[col.shift.strip] = col.map {|s| s.strip}
+}
+
+key = []
+chars = str.chars
+bias = 0
+str.chars.each.with_index do |c, i|
+  if romaji.keys.include?(c)
+    key << romaji[c]
+  else
+    tmp = romaji[(chars[i-1-bias] + c).chars.uniq.join]
+
+    if tmp.nil?
+      key << [romaji[chars[i+1-bias]][0].chars[0]]
+    else
+      key[-1] = tmp
+      chars[i-1-bias] += c; chars.delete_at(i-bias)
+      bias += 1
+    end
+  end
+end
+p str
+p key.map { |c| c[0] }.join
+p key
+p key.map { |c| romaji.find {|k,v| v[0] == c[0] }[0] }.join.tr('ぁ-ん','ァ-ン')
