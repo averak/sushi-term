721d45dee5fe9d13a152ab2e83e80357bba5b0cc
AjxLab <abe12@mccc.jp>
2020-01-27 02:04:34 +0900

    print_board

--- sushiterm.rb
+++ sushiterm.rb
@@ -31,10 +31,32 @@ class SushiTerm
 
   def exec
     ## -----*----- 処理実行 -----*----- ##
+    loop do
+      @time = @limit.dup         # 残り時間
+      timer 60*100               # 残り時間のタイマー
+      @quest = @sentences.sample # 現在の問題文
+      @quest[:input] = ''
+    end
   end
 
 
   private
+
+
+  def timer(frame_rate)
+    ## -----*----- タイマー -----*----- ##
+    Timer::set_frame_rate(frame_rate)
+    Timer::timer {
+      @time -= 0.01
+      # 描画
+      print_board(
+        build_timebar(@time),
+        @quest[:text],
+        build_outstr(@quest[:romaji]),
+        @quest[:input]
+      )
+    }
+  end
 
 
   def print_board(*msg)
@@ -43,10 +65,27 @@ class SushiTerm
   end
 
 
-  def make_outstr(romaji, n_chars=0)
+  def build_outstr(romaji, n_chars=0)
     ## -----*----- 出力文字を生成 -----*----- ##
     # romaji : 出力文字のローマ字配列
     # words  : 入力された文字数
+  end
+
+
+  def build_timebar(time)
+    ## -----*----- 残り時間のバー生成 -----*----- ##
+    width = `tput cols`.to_i - 18
+    return '' if (width * time / @limit).to_i <= 0.0
+
+    bar = '■' * (width * time / @limit).to_i
+
+    if time > @limit * 2/3
+      return "\e[32m#{bar}\e[0m"
+    elsif time > @limit / 3
+      return "\e[33m#{bar}\e[0m"
+    else
+      return "\e[31m#{bar}\e[0m"
+    end
   end
 end
 
