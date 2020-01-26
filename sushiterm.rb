e8223c2e972ab69ded96506152cc856169cffd61
AjxLab <abe12@mccc.jp>
2020-01-27 01:11:16 +0900

    アーキテクチャを決定

--- sushiterm.rb
+++ sushiterm.rb
@@ -12,5 +12,33 @@ class SushiTerm
   def initialize
     ## -----*----- コンストラクタ -----*----- ##
     @cons = Console.new './config/outfmt.txt'
+    @limit = 5.0
   end
+
+
+  def exec
+    ## -----*----- 処理実行 -----*----- ##
+  end
+
+
+  private
+
+
+  def print_board(*msg)
+    ## -----*----- 画面出力 -----*----- ##
+    @cons.draw(*msg)
+  end
+
+
+  def make_outstr(romaji, n_chars=0)
+    ## -----*----- 出力文字を生成 -----*----- ##
+    # romaji : 出力文字のローマ字配列
+    # words  : 入力された文字数
+  end
+end
+
+
+if __FILE__ == $0
+  obj = SushiTerm.new
+  obj.exec
 end
